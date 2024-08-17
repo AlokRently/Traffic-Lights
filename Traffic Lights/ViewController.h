2e7fb022cf7571923bb395507d6ffd196d5df5b8
Kumar Alok <kumaralok.s@rently.com>
2024-08-17 23:10:55 +0530

    Complete the project

--- Traffic Lights/ViewController.h
+++ Traffic Lights/ViewController.h
@@ -7,7 +7,20 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController {
+    NSTimer *timer;
+    NSTimer *scoreTimer;
+    
+    int timerInt;
+    int scoreInt;
+}
+
+@property (weak, nonatomic) IBOutlet UIButton *startButton;
+@property (weak, nonatomic) IBOutlet UILabel *scoreLabel;
+
+@property (weak, nonatomic) IBOutlet UIImageView *trafficLight;
+
+- (IBAction)startGame:(id)sender;
 
 
 @end
